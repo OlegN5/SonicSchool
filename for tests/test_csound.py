27e267e876f6c6b10be1f48507bfc519d2b36023
OlegN5 <6370736@gmail.com>
2021-02-05 11:47:14 +0300

    Create test_csound.py

---
+++ for tests/test_csound.py
@@ -0,0 +1,29 @@
+import sys
+import ctcsound
+
+csd_text = '''
+  <CsoundSynthesizer>
+  <CsOptions>
+    -odac
+  </CsOptions>
+  <CsInstruments>
+  instr 1
+    out(linen(oscili(p4,p5),0.1,p3,0.1))
+  endin
+  </CsInstruments>
+  <CsScore>
+  i1 0 5 1000 440
+  </CsScore>
+  </CsoundSynthesizer>'''
+
+cs = ctcsound.Csound()
+result = cs.compileCsdText(csd_text)
+result = cs.start()
+while True:
+    result = cs.performKsmps()
+    if result != 0:
+        break
+result = cs.cleanup()
+cs.reset()
+del cs
+sys.exit(result)
