1bebc26503dc688e24906060a046f77e4cbf81e7
OlegN5 <6370736@gmail.com>
2021-02-05 04:40:17 +0300

    Update views.py

--- repeater/views.py
+++ repeater/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
+#import librosa
+#import numpy
+#import IPython.display as ipd
 # Create your views here.
 
 def home(request):
+
     context = {
         'wav': 'a_-1.wav'
     }
+
+
     return render(request, 'repeater/home.html', context=context)
 
 def get_recorded_audio(request):
@@ -16,3 +22,14 @@ def get_recorded_audio(request):
     messages.success(request, 'Файл отправлен на сервер!')
     #return render(request, 'repeater/home.html', context=context)
     return redirect('home')
+
+'''def load_audio():
+    audio_data = 'resources/a_-1.wav'
+    librosa.load(audio_data, sr=44100)
+    ipd.Audio(audio_data) # проиграть
+    x, sr = librosa.load(audio_data, sr=44100) # может и так сразу здесь задать передискретизацию
+
+    print(type(x), type(sr))
+    #<class 'numpy.ndarray'> <class 'int'>
+    print(x.shape, sr)
+    #(94316,) 22050'''
