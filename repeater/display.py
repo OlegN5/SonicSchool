a7ce4e451f336a3ffccc24b53f96cb2504deeea3
Dmitry Kudryashov <dmitry-kud@yandex.ru>
2021-06-06 17:07:48 +0300

    Updated `requirements.txt` to correspond to the dependencies

--- repeater/display.py
+++ repeater/display.py
@@ -1,7 +1,7 @@
-import plotly
-import plotly.graph_objs as go
-import plotly.express as px
-from plotly.subplots import make_subplots
+#import plotly
+#import plotly.graph_objs as go
+#import plotly.express as px
+#from plotly.subplots import make_subplots
 
 import numpy as np
 import pandas as pd
--- sonic/settings.py
+++ sonic/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SONICSCHOOL_DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.104']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
