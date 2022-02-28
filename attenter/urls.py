f412cf269607f04754ad6449e39c27d1f2074d6f
OlegN5 <6370736@gmail.com>
2022-02-28 12:24:28 +0300

    correct urls

--- attenter/urls.py
+++ attenter/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-from attenter import views as attenter_views
+# from attenter import views as attenter_views
 
 urlpatterns = [
     path ('', index)
