d72156a2ea9e698c5f60b589ac8212753570e42f
OlegN5 <6370736@gmail.com>
2022-04-03 09:00:36 +0300

    correct news

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path ('', PostListView.as_view(), name='news'),
     path ('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path ('post/new/', PostCreateView.as_view(), name='post-create'),
     path ('test/', test)
-    ]
+    ] 
