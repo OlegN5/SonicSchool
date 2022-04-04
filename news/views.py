9b94fea023d3976532b2c7ab89c022638d2aca1b
OlegN5 <6370736@gmail.com>
2022-04-04 15:57:09 +0300

    new news

--- news/views.py
+++ news/views.py
@@ -1,10 +1,19 @@
+from audioop import reverse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView)
+    CreateView,
+    UpdateView,
+    DeleteView)
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from django.contrib.auth.decorators import login_required
+
+
+
 # Create your views here.
 
 # posts = [{
@@ -35,13 +44,39 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+
+# @login_required
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form): #???
         form.instance.author = self.request.user #???
         return super().form_valid(form)  #???
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form): #???
+        form.instance.author = self.request.user #???
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/news'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 
 
