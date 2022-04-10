1be39253c8c7a4746e089e4c28bd8da2464879ea
OlegN5 <6370736@gmail.com>
2022-04-11 02:34:52 +0300

    fix pizz sound

--- intonation/static/intonation/js/script.js
+++ intonation/static/intonation/js/script.js
@@ -1,4 +1,3 @@
-var group = new Pizzicato.Group()
 var sound = []
 if (Pizzicato.context.state !== "running") {
   Pizzicato.context.resume();
@@ -29,24 +28,22 @@ const app = Vue.createApp({
         name: "Intonation",
         info: "Тренажер восприятие интонационных связей"
       },
-      items:[
-        
+      items:[      
       ],
       templates:[
-        {name: 'До', file: '0'},
-        {name: 'До#', file: '2'},
-        {name: 'Ре', file: '4'},
-        {name: 'Ре#', file: '6'},
-        {name: 'Ми', file: '8'},
-        {name: 'Фа', file: '10'},
-        {name: 'Фа#', file: '12'},
-        {name: 'Соль', file: '14'},
-        {name: 'Соль#', file: '16'},
-        {name: 'Ля', file: '18'},
-        {name: 'Ля#', file: '20'},
-        {name: 'Си', file: '22'},
+        {name: 'До', file: '0', frequency: '261.626'},
+        {name: 'До#', file: '2', frequency: '277.183'},
+        {name: 'Ре', file: '4', frequency: '293.665'},
+        {name: 'Ре#', file: '6', frequency: '311.127'},
+        {name: 'Ми', file: '8', frequency: '329.628'},
+        {name: 'Фа', file: '10', frequency: '349.228'},
+        {name: 'Фа#', file: '12', frequency: '369.994'},
+        {name: 'Соль', file: '14', frequency: '391.995'},
+        {name: 'Соль#', file: '16', frequency: '415.305'},
+        {name: 'Ля', file: '18', frequency: '440'},
+        {name: 'Ля#', file: '20', frequency: '466.164'},
+        {name: 'Си', file: '22', frequency: '493.883'},
       ]
-
       }
     },
   
@@ -57,91 +54,110 @@ const app = Vue.createApp({
     
   },
   created: function () {
-
- 
-
     nItems = getRandomIntInclusive(2, 5)
     var a =[]
     for (var i = 0; i < nItems+1; i++) {
       m = getRandomIntInclusive(0, 11)
       this.items.push(this.templates[m])
-
       
-
+      
       sound[i] = new Pizzicato.Sound(
         {
           source: "file",
-          options: { path: static_url + "intonation/sounds/" + this.items[i].file + '.wav'},
+          options: { path: static_url + "intonation/sounds/" + this.items[i].file + '.wav' },
+          detached: true
         },
         () => {
-          console.log("sound file loaded!", i);
           
-
+          console.log("sound file loaded!");
           this.$el.querySelector('.loaded').classList.remove('loaded')
-          // console.log('this.$el',this.$el.querySelector('#li0'))
-           
-        }
+          for(i=0;i>sound.length;i++) {
+            sound[i].on('end', () => {
+            sound[i].detached = true
+          })
+          sound[i].on('stop', () => {
+            sound[i].detached = true
+          })
+          }
 
-        
+          // console.log('this.$el',this.$el.querySelector('#li0'))      
+        }
       );
+        console.log('sound[i]',sound[i])
+     
+
+
+      // sound[i] = new Pizzicato.Sound(
+      //   {
+      //     source: "wave",
+      //     options: { 
+      //       frequency: this.items[i].frequency},
+      //       type: 'sawtooth'
+      //   }
+      // );
+      // this.$el.querySelector('.loaded').classList.remove('loaded')
       
-      sound[i].volume = getRandomIntInclusive(30, 100)/100
+
+      sound[i].volume = getRandomIntInclusive(40, 60)/100
       this.items[i].volume = sound[i].volume
       console.log(sound[i].volume)
-
       // sound[i].play()
-   }
-
-  
-   
-
-   
-  },
+   }},
   computed: {
     //кэшируемые методы
     //когда нужно вычислить один раз и использовать
     //пока что-то не поменялось
-  },
-  
+  }, 
   methods: {
     play() {
-
-      
-    console.log(this.$el)
-
-
-      for (var i=0;i < sound.length; i++) {
-       
-        sound[i].play(i*1.5 , 0)
-
-       
-
-
-      }
-      
       
 
+      sound.forEach((s, index) => {
+        s.detached = false
+        // s.relese = .8
+        s.play(index*1 , 0)
+        s.detached = true
+        
+      })
 
-      
 
+
+      // for (var i=0; i<sound.length; i++) {
+      //   sound[i].detached = false
+      //   sound[i].relese = .8
+      //   sound[i].play(i*1 , 0)
+      //   p[i] = setTimeout (function ()  {
+      //     console.log('stop', i)
+      //     sound[i].stop()
+      //   }, 1000)
+      // }
     },
 
     volumeChange() {    
       Pizzicato.volume = this.$el.querySelector('#MasterVolume').value
+      console.log('sound',sound)
     },
    
-    playItem: (function(e){
-
-
-      if (Pizzicato.context.state !== "running") {
-        Pizzicato.context.resume();
-      }
+    playItem: ((e) => {
       console.log(sound[e.target.value].volume)
-      sound[e.target.value].play();
+      const a = e.target.value
+      sound[a].detached = false
+      sound[a].play();
+      sound[a].detached = true
+       
+      
+      setTimeout (function ()  {
+        console.log('stop')
+        sound[a].stop()
+      }, 1000)
+      
+      // if (Pizzicato.context.state !== "running") {
+      //   Pizzicato.context.resume();
+      // }
       
     }),
 
-    dragstart: (function(e){
+    dragstart: ((e) => {
       e.target.classList.add(`selected`);
     }),
 
@@ -152,21 +168,14 @@ const app = Vue.createApp({
 
     dragover: (function(e){
       e.preventDefault();
-      
-      // const activeElement = tasksListElement;
       const activeElement = this.$el.querySelector(`.selected`)
       const currentElement = e.target;
-
-      // console.log('activeElement', activeElement);
-      // console.log('currentElement', currentElement);
       const isMoveable = activeElement !== currentElement &&
       currentElement.classList.contains(`tasks__item`);
-        
       if (!isMoveable) {
         return;
       } 
       const nextElement = getNextElement(e.clientY, currentElement);
-      
       if (
         nextElement && 
         activeElement === nextElement.previousElementSibling ||
@@ -174,7 +183,6 @@ const app = Vue.createApp({
       ) {
         return;
       }
-       
       this.$el.querySelector(`.tasks__list`).insertBefore(activeElement, nextElement);
     }),
   }
